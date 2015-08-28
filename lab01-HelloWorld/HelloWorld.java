93fb2f4b2249eeb4adb354d87798a067c20c5a87
tht217 <tht217@lehigh.edu>
2015-08-28 16:12:59 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+////////////////////////////////////////////////////////////////////////////////
+//Thomas Troxell
+//
+//Hello World Java Program
+//
+//defining a class
+public class HelloWorld{
+    
+//adding a method
+    public static void main(String[] args) {
+        
+//printing
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("#                      #");
+        System.out.println("#  Hello World 2015!!  #");
+        System.out.println("#                      #");
+        System.out.println("########################");
+        System.out.println("########################");
+        System.out.println("test");
+        
+
+    }
+}
