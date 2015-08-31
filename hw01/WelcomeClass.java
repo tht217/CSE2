cb8267edea1901b5b9d02200465cbce96d1cfb23
tht217 <tht217@lehigh.edu>
2015-08-31 22:44:40 +0000

    Wrote hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+//Thomas Troxell
+//
+//Hello World Java Program
+//
+//defining a class
+public class WelcomeClass{
+    
+//adding a method
+    public static void main(String[] args) {
+        
+//printing
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("<-T--H--T--2--1--7->");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("In my freshman year, I devoted my summer to training");
+        System.out.println("and trying out for the Lehigh track team. One time,");
+        System.out.println(" I did a workout at 7,000 ft altitude and passed out!");
+        
+
+    }
+}
